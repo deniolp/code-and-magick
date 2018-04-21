768064103989b1440a84d5147ff5e582914ef674
Denis Popov <deniolp@gmail.com>
2018-04-21 13:17:19 +0300

    Создание модуля backend.js, прописывание в нем методов load и save с логикой сообщения ошибок

---
+++ js/backend.js
@@ -0,0 +1,47 @@
+'use strict';
+
+(function () {
+
+  var URL = 'https://js.dump.academy/code-and-magick/data';
+
+  window.backend = {
+    save: function (data, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad();
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.open('POST', URL);
+      xhr.send(data);
+    },
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.open('GET', URL);
+      xhr.send();
+    }
+  };
+})();
