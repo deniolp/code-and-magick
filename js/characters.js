1fc13b55de849add996393d6f1d2a1cc5ee4452b
Denis Popov <deniolp@gmail.com>
2018-04-28 11:42:13 +0300

    Реализация изменения цвета куртки по клику (по аналогии с изменением цвета глаз)

--- js/characters.js
+++ js/characters.js
@@ -18,12 +18,27 @@
     '#e6e848'
   ];
 
+  var COAT_COLORS = [
+    'rgb(146, 100, 161)',
+    'rgb(215, 210, 55)',
+    'rgb(241, 43, 107)',
+    'rgb(101, 137, 164)',
+    'rgb(0, 0, 0)',
+    'rgb(215, 210, 55)',
+    'rgb(56, 159, 117)',
+    'rgb(241, 43, 107)'
+  ];
+
   var generateEyesColor = function () {
     return EYES_COLORS[window.utils.generateRandomNumber(0, EYES_COLORS.length - 1)];
   };
 
   var generateFireballColor = function () {
     return FIREBALL_COLORS[window.utils.generateRandomNumber(0, FIREBALL_COLORS.length - 1)];
+  };
+
+  var generateCoatColor = function () {
+    return COAT_COLORS[window.utils.generateRandomNumber(0, COAT_COLORS.length - 1)];
   };
 
   var successHandler = function (data) {
@@ -50,6 +65,8 @@
   var eyesColorInputElement = setupElement.querySelector('input[name = eyes-color]');
   var setupFireballWrapElement = setupElement.querySelector('.setup-fireball-wrap');
   var fireballColorInputElement = setupElement.querySelector('input[name = fireball-color]');
+  var setupWizardCoatElement = setupElement.querySelector('.wizard-coat');
+  var coatColorInputElement = setupElement.querySelector('input[name = coat-color]');
 
   var wizards = [];
 
@@ -61,6 +78,11 @@
   setupFireballWrapElement.addEventListener('click', function () {
     fireballColorInputElement.value = generateFireballColor();
     setupFireballWrapElement.style.backgroundColor = fireballColorInputElement.value;
+  });
+
+  setupWizardCoatElement.addEventListener('click', function () {
+    setupWizardCoatElement.style.fill = generateCoatColor();
+    coatColorInputElement.value = setupWizardCoatElement.style.fill;
   });
 
   window.backend.load(successHandler, errorHandler);
