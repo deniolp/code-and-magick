2eb5bbc997fd38b62d6bb0066a68edd08b816bb8
Denis Popov <deniolp@gmail.com>
2018-04-28 12:28:35 +0300

    Реализация сортировки похожих волшебников

--- js/characters.js
+++ js/characters.js
@@ -43,11 +43,12 @@
 
   var successHandler = function (data) {
     wizards = data;
-    window.render(wizards);
+    updateWizards();
   };
 
   var errorHandler = function (errorMessage) {
     var node = document.createElement('div');
+
     node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
     node.style.position = 'absolute';
     node.style.left = 0;
@@ -56,6 +57,39 @@
 
     node.textContent = errorMessage;
     document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  var getRank = function (wizard) {
+    var rank = 0;
+
+    if (wizard.colorCoat === coatColor) {
+      rank += 2;
+    }
+    if (wizard.colorEyes === eyesColor) {
+      rank += 1;
+    }
+
+    return rank;
+  };
+
+  var namesComparator = function (left, right) {
+    if (left > right) {
+      return 1;
+    } else if (left < right) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
+  var updateWizards = function () {
+    window.render(wizards.sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = namesComparator(left.name, right.name);
+      }
+      return rankDiff;
+    }));
   };
 
   var setupElement = document.querySelector('.setup');
@@ -70,9 +104,15 @@
 
   var wizards = [];
 
+  var eyesColor;
+  var coatColor;
+
   setupWizardEyesElement.addEventListener('click', function () {
-    setupWizardEyesElement.style.fill = generateEyesColor();
-    eyesColorInputElement.value = setupWizardEyesElement.style.fill;
+    var newColor = generateEyesColor();
+    setupWizardEyesElement.style.fill = newColor;
+    eyesColorInputElement.value = newColor;
+    eyesColor = newColor;
+    updateWizards();
   });
 
   setupFireballWrapElement.addEventListener('click', function () {
@@ -81,8 +121,11 @@
   });
 
   setupWizardCoatElement.addEventListener('click', function () {
-    setupWizardCoatElement.style.fill = generateCoatColor();
-    coatColorInputElement.value = setupWizardCoatElement.style.fill;
+    var newColor = generateCoatColor();
+    setupWizardCoatElement.style.fill = newColor;
+    coatColorInputElement.value = newColor;
+    coatColor = newColor;
+    updateWizards();
   });
 
   window.backend.load(successHandler, errorHandler);
