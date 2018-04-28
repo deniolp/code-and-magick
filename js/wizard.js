c13bfc40cb17d53fb4d770b9e9ef329c85686483
Denis Popov <deniolp@gmail.com>
2018-04-28 13:42:38 +0300

    Вынесение модуля настройки мага от модуля загрузки похожих и переименование модуля загрузки похожих волшебников

--- js/wizard.js
+++ js/wizard.js
@@ -41,59 +41,7 @@
     return COAT_COLORS[window.utils.generateRandomNumber(0, COAT_COLORS.length - 1)];
   };
 
-  var successHandler = function (data) {
-    wizards = data;
-    updateWizards();
-  };
-
-  var errorHandler = function (errorMessage) {
-    var node = document.createElement('div');
-
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-    node.style.position = 'absolute';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '35px';
-
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
-  };
-
-  var getRank = function (wizard) {
-    var rank = 0;
-
-    if (wizard.colorCoat === coatColor) {
-      rank += 2;
-    }
-    if (wizard.colorEyes === eyesColor) {
-      rank += 1;
-    }
-
-    return rank;
-  };
-
-  var namesComparator = function (left, right) {
-    if (left > right) {
-      return 1;
-    } else if (left < right) {
-      return -1;
-    } else {
-      return 0;
-    }
-  };
-
-  var updateWizards = function () {
-    window.render(wizards.sort(function (left, right) {
-      var rankDiff = getRank(right) - getRank(left);
-      if (rankDiff === 0) {
-        rankDiff = namesComparator(left.name, right.name);
-      }
-      return rankDiff;
-    }));
-  };
-
   var setupElement = document.querySelector('.setup');
-  var form = setupElement.querySelector('.setup-wizard-form');
 
   var setupWizardEyesElement = setupElement.querySelector('.wizard-eyes');
   var eyesColorInputElement = setupElement.querySelector('input[name = eyes-color]');
@@ -102,17 +50,16 @@
   var setupWizardCoatElement = setupElement.querySelector('.wizard-coat');
   var coatColorInputElement = setupElement.querySelector('input[name = coat-color]');
 
-  var wizards = [];
-
-  var eyesColor;
-  var coatColor;
+  var wizard = {
+    eyesPressHandler: function () {},
+    coatPressHandler: function () {}
+  };
 
   setupWizardEyesElement.addEventListener('click', function () {
     var newColor = generateEyesColor();
     setupWizardEyesElement.style.fill = newColor;
     eyesColorInputElement.value = newColor;
-    eyesColor = newColor;
-    updateWizards();
+    wizard.eyesPressHandler(newColor);
   });
 
   setupFireballWrapElement.addEventListener('click', function () {
@@ -124,16 +71,8 @@
     var newColor = generateCoatColor();
     setupWizardCoatElement.style.fill = newColor;
     coatColorInputElement.value = newColor;
-    coatColor = newColor;
-    updateWizards();
+    wizard.coatPressHandler(newColor);
   });
 
-  window.backend.load(successHandler, errorHandler);
-
-  form.addEventListener('submit', function (evt) {
-    window.backend.save(new FormData(form), function () {
-      setupElement.classList.add('hidden');
-    }, errorHandler);
-    evt.preventDefault();
-  });
+  window.wizard = wizard;
 })();
